e6454b0f8b2f41edafe561d1261cec3f601e558e
KhaledT7 <malcomkhaledcheick.kojoktarraf@ynov.com>
2022-10-05 17:10:39 +0200

    add comment

--- main.go
+++ main.go
@@ -11,40 +11,40 @@ import (
 
 func main() {
 
-	data, err := os.Open("Hangman.txt")
+	data, err := os.Open("Hangman.txt") // Open the file
 
-	if err != nil {
-		fmt.Println(err)
+	if err != nil { // Check for errors
+		fmt.Println(err) // If there is an error, print it
 	}
-	scanner := bufio.NewScanner(data)
+	scanner := bufio.NewScanner(data) // Create a new scanner
 
-	scanner.Split(bufio.ScanWords)
+	scanner.Split(bufio.ScanWords) // Split the file by words
 
-	tab := make([]string, 0)
+	tab := make([]string, 0) // Create a slice of strings
 
-	for scanner.Scan() {
-		tab = append(tab, scanner.Text())
+	for scanner.Scan() { // Scan the file
+		tab = append(tab, scanner.Text()) // Append the words to the slice
 	}
 
-	fmt.Println(tab[0])
-	fmt.Println(tab[len(tab)-1])
+	fmt.Println(tab[0])          // Print the first word
+	fmt.Println(tab[len(tab)-1]) // Print the last word
 
 	for index, a := range tab {
-		if a == "before" {
-			result, _ := strconv.Atoi(tab[index+1])
-			fmt.Println(tab[result-1])
+		if a == "before" { // If the word is "before"
+			result, _ := strconv.Atoi(tab[index+1]) // Convert the next word to an integer
+			fmt.Println(tab[result-1])              // Print the word at the index of the integer
 		}
-		if a == "now" {
-			are := tab[index-1]
-			result1 := int(are[1]) / len(tab)
-			fmt.Println(tab[result1-1])
+		if a == "now" { // If the word is "now"
+			are := tab[index-1]               // Get the word before "now"
+			result1 := int(are[1]) / len(tab) // Divide the second letter of the word by the length of the slice
+			fmt.Println(tab[result1-1])       // Print the word at the index of the result
 		}
 	}
 
-	min := 0
-	max := 1000
-	rand.Seed(time.Now().Unix())
-	fmt.Println("The random number is", rand.Intn(max-min)+min)
+	min := 0                                                    // Set the minimum value for the random number
+	max := 1000                                                 // Set the maximum value for the random number
+	rand.Seed(time.Now().Unix())                                // Seed the random number generator
+	fmt.Println("The random number is", rand.Intn(max-min)+min) // Print the random number
 
-	data.Close()
+	data.Close() // Close the file
 }
