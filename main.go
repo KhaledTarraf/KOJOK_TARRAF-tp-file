1daaf4c7a57e2e6cbdf7b78755564dbdbd889dd1
KhaledT7 <malcomkhaledcheick.kojoktarraf@ynov.com>
2022-10-05 16:59:50 +0200

    found secret order

--- main.go
+++ main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	data, err := os.ReadFile("Hangman.txt")
+
+	data, err := os.Open("Hangman.txt")
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(data))
+	scanner := bufio.NewScanner(data)
+
+	scanner.Split(bufio.ScanWords)
+
+	tab := make([]string, 0)
+
+	for scanner.Scan() {
+		tab = append(tab, scanner.Text())
+	}
+
+	fmt.Println(tab[0])
+	fmt.Println(tab[len(tab)-1])
+
+	for index, a := range tab {
+		if a == "before" {
+			result, _ := strconv.Atoi(tab[index+1])
+			fmt.Println(tab[result-1])
+		}
+		if a == "now" {
+			are := tab[index-1]
+			result1 := int(are[1]) / len(tab)
+			fmt.Println(tab[result1-1])
+		}
+	}
+
+	data.Close()
 }
